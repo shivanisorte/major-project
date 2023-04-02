d71744060c67251198d842aa1d29ee4fd959d05d
Shivani Sorte <sssorte1428@gmail.com>
2023-04-02 17:21:47 +0530

    get started

--- client/src/components/GetStarted.js
+++ client/src/components/GetStarted.js
@@ -1,51 +1,51 @@
 import React from 'react'
 import AppNav from './AppNav'
-import img from './../assets/gSCoordinator.png'
-import { Box, Flex, Heading,Image,Button,Stack} from "@chakra-ui/react";
+import { Box, Flex, Heading,Image,Button,Stack, Center} from "@chakra-ui/react";
 import { BiChevronRight } from "react-icons/bi";
-import { Link } from "react-router-dom";
   
 
-const GetStarted = () => {
+const GetStarted = ({ image,buttonText, heading }) => {
 
   return (
     <div>
         <AppNav/>
+       
         <Flex
       minH="90vh"
       justify="center"
       direction="column"
       
+
     >
       <Box maxW="lg" mx="auto" textAlign="center">
-        <Heading fontSize={{ base: "3xl", md: "3xl", lg: "4xl" }}>
-          Upload data to get started.
+        <Heading fontSize={{ base: "3xl", md: "3xl", lg: "3xl" }}>
+          {heading}
         </Heading>
-        <Image src={img} alt="get started coordinator img" my={4} ml={4} />
+        <Image src={image} alt="get started coordinator img" my={3} ml={4} />
        
 
-        <Stack direction={["column", "column", "row", "row"]} spacing={"20px"}>
+        <Stack direction={["column", "row", "row", "row"]} spacing={"20px"} align={'center'}>
           {" "}
-          <Link to="/student">
+  
+  
             <Button
               width="2xs"
               rightIcon={<BiChevronRight />}
               colorScheme="purple"
               fontSize={["l", "l", "xl", "xl"]}
             >
-              Upload Student Data
+              {buttonText[0]}
             </Button>
-          </Link>
-          <Link to="/guide">
+
             <Button
               width="2xs"
               rightIcon={<BiChevronRight />}
               colorScheme="purple"
               fontSize={["l", "l", "xl", "xl"]}
             >
-              Upload Guide Data
+              {buttonText[1]}
             </Button>
-          </Link>
+            
           
         </Stack>
        
