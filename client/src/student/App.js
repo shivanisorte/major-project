f5ccd8b5aa17a5487c801e1d6e53c6a49af7a4cc
Shivani Sorte <sssorte1428@gmail.com>
2023-04-08 00:52:45 +0530

    student form a and get started page done

--- client/src/student/App.js
+++ client/src/student/App.js
@@ -2,10 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import DefaultPage from "./pages/DefaultPage";
 import Login from "../components/Login";
-import GetStarted from "../components/GetStarted";
-
 import logo from './../assets/student.png'
 import gsimage from './../assets/gSStudent.png'
+
+import FormA from "./pages/FormA";
+
+import GetStartedStud from './pages/GetStartedStud';
+
+
 
 function App() {
   return (
@@ -14,8 +18,8 @@ function App() {
         <Route path="/" element={<DefaultPage></DefaultPage>}></Route>
         <Route path="/login" element={<Login role={'Student'} image={logo}></Login>}></Route>
         <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
-        <Route path="/getStarted" element={<GetStarted heading={'Your project has not been finalized yet'} image={gsimage} buttonText={['Go To Project Hub','Submit 3 ideas']}></GetStarted>}></Route>
-        
+        <Route path="/getStarted" element={<GetStartedStud heading={'Your project has not been finalized yet'} image={gsimage} buttonText={['Go To Project Hub','Submit 3 ideas']}></GetStartedStud>}></Route>
+        <Route path="/formA" element={<FormA></FormA>}></Route>
       </Routes>
     </>
   );
---
+++ client/src/student/pages/GetStartedStud.js
@@ -0,0 +1,56 @@
+import React from "react";
+import AppNav from "./../../components/AppNav";
+import { Box, Flex, Heading, Image, Button, Stack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { BiChevronRight } from "react-icons/bi";
+
+const GetStartedStud = ({ image, buttonText, heading }) => {
+  return (
+    <div>
+      <AppNav />
+
+      <Flex minH="90vh" justify="center" direction="column">
+        <Box maxW="lg" mx="auto" textAlign="center">
+          <Heading fontSize={{ base: "3xl", md: "3xl", lg: "3xl" }}>
+            {heading}
+          </Heading>
+          <Image
+            maxWidth={["90vw", "100%"]}
+            src={image}
+            alt="get started coordinator img"
+            my={3}
+            ml={4}
+          />
+
+          <Stack
+            direction={["column", "row", "row", "row"]}
+            spacing={"20px"}
+            align={"center"}
+          >
+            {" "}
+            <Button
+              width="2xs"
+              rightIcon={<BiChevronRight />}
+              colorScheme="purple"
+              fontSize={["l", "l", "xl", "xl"]}
+            >
+              {buttonText[0]}
+            </Button>
+            <Link to='../formA'>
+            <Button
+              width="2xs"
+              rightIcon={<BiChevronRight />}
+              colorScheme="purple"
+              fontSize={["l", "l", "xl", "xl"]}
+            >
+              {buttonText[1]}
+            </Button>
+            </Link>
+          </Stack>
+        </Box>
+      </Flex>
+    </div>
+  );
+};
+
+export default GetStartedStud;
