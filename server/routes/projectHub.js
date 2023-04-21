1bde431b4e6cb52ac5280f04778528fdf2d7c316
Shivani Sorte <sssorte1428@gmail.com>
2023-04-21 13:03:31 +0530

    put and delete request

--- server/routes/projectHub.js
+++ server/routes/projectHub.js
@@ -53,6 +53,30 @@ router.post("/", async (req, res) => {
 
 
 });
+
+    router.put('/:id', async (req, res) => {
+        const projectId = req.params.id;
+        const { title, projectType, domain, description, technologies, contact, otherDetails, status } = req.body;
+    
+        try {
+        const project = await ProjectHub.findByIdAndUpdate(projectId, { title, projectType, domain, description, technologies, contact, otherDetails, status }, { new: true });
+        res.json(project);
+        } catch (error) {
+        res.status(500).json({ message: error.message });
+        }
+    });
+
+    router.delete('/:id', async (req, res) => {
+      
+        try {
+          const project = await ProjectHub.findByIdAndDelete(req.params.id);
+          res.json(project);
+        } catch (error) {
+          res.status(500).json({ message: error.message });
+        }
+      });
+
+
   
   module.exports = router;
   
