463f7e5a81222f60856b896f377fd5819b538d7b
Shivani Sorte <sssorte1428@gmail.com>
2023-04-22 06:02:54 +0530

    created application form

--- client/src/student/components/ProjectModal.js
+++ client/src/student/components/ProjectModal.js
@@ -9,10 +9,44 @@ import {
   Text,
   ModalFooter,
   Button,
+  FormControl,
+  FormLabel,
+  Textarea,
 } from "@chakra-ui/react";
 
+import { useState } from "react";
+
 const ProjectModal = ({ project, isOpen, toggleModal }) => {
+  const [isApplyModalOpen, setIsApplyModalOpen] = useState(false);
+  const [teamBackground, setTeamBackground] = useState('');
+  const [pastProjects, setPastProjects] = useState('');
+  const [projectProposal, setProjectProposal] = useState('');
+
+  const isApplyModalClose = () => {
+    setIsApplyModalOpen(false);
+  };
+
+  const handleApply=()=>{
+    toggleModal();
+    setIsApplyModalOpen(true);
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const projectData = {
+      teamBackground,
+      pastProjects,
+      projectProposal
+    };
+
+    console.log(projectData)
+  }
+    
+
   return (
+    <>
+
     <Modal isOpen={isOpen} onClose={toggleModal} size="xl">
       <ModalOverlay />
       <ModalContent>
@@ -41,12 +75,48 @@ const ProjectModal = ({ project, isOpen, toggleModal }) => {
           </Box>
         </ModalBody>
         <ModalFooter>
-          <Button colorScheme="purple" mr={3} onClick={toggleModal}>
+          <Button colorScheme="purple" mr={3} 
+          onClick={handleApply}
+          >
             Apply
           </Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
+
+
+    <Modal isOpen={isApplyModalOpen} onClose={isApplyModalClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Your Application </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl id="team-background" isRequired>
+              <FormLabel>Team Background</FormLabel>
+              <Textarea placeholder="Introduce each team member with their background and skills." />
+            </FormControl>
+            <FormControl id="past-projects" isRequired>
+              <FormLabel>Relevant Past Projects/Experience</FormLabel>
+              <Textarea placeholder="Enter relevant past projects/experience that your team has." />
+            </FormControl>
+            <FormControl id="project-proposal" isRequired>
+              <FormLabel>Project Proposal</FormLabel>
+              <Textarea placeholder="Enter a short project proposal describing your objective and goals and how you plan to achieve them." />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="purple" mr={3} onClick={handleSubmit}>
+              Submit
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
+
+
+</>
+
   );
 };
 
